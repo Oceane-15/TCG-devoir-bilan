a957d8aa53c7653cd3e5d0a651ea61ffea16bb9f
Océane <docasaloceane05@gmail.com>
2026-08-27 10:54:10 +0200

    Ajout db.php

---
+++ config/db.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database {
+    private $host = 'localhost';
+    private $dbname = 'tcg_shop';
+    private $username = 'root';
+    private $password = '';
+    private ?PDO $conn = null;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";charset=utf8", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Erreur de connexion : " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
